eed2900850561d77590c1b1309d1338a2f074d70
Gaetano Sabino <gaetano.sabino@gmail.com>
2019-05-17 19:44:40 -0500

    carosel bootstrap risolto

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  foto = new Array(10).fill('./assets/img/android-chrome-512x512.png');
+
   constructor() { }
 
   ngOnInit() {
+
+    console.log(this.foto);
+
   }
 
 }
